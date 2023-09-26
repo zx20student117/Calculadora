8d875762de354a4c52b161dc345b779a818ffa50
zx20student117 <zx20student117@colegiovalledelmiro.es>
2023-09-26 10:11:06 +0200

    Calculadora v1. (Sumar)

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+"""
+Calculadora en python
+"""
+
+num_1 = int(input("Ingrese el primer numero: "))
+
+num_2 = int(input("Ingrese el segundo numero: "))
+
+def sumar(n1, n2):
+    return n1 + n2
+
+resultado = sumar(num_1, num_2)
+
+print("El resultado es: ", resultado)
