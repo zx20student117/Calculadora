c741c34bc2fc72826c7ebf4fe4303672a991cf78
zx20student117 <zx20student117@colegiovalledelmiro.es>
2023-10-03 09:00:35 +0200

    Calculadora v1.2 (Dividir)

--- calculadora.py
+++ calculadora.py
@@ -12,10 +12,24 @@ def sumar(n1, n2):
 def restar(n1, n2):
     return n1 - n2
 
+def multiplicar(n1, n2):
+    return n1 * n2
+
+def dividir(n1, n2):
+    return n1 / n2
+
 resultado_1 = sumar(num_1, num_2)
 
 resultado_2 = restar(num_1, num_2)
 
+resultado_3 = multiplicar(num_1, num_2)
+
+resultado_4 = dividir(num_1, num_2)
+
 print("El resultado es: ", resultado_1)
 
 print("El resultado es: ", resultado_2)
+
+print("El resultado es: ", resultado_3)
+
+print("El resultado es: ", resultado_4)
